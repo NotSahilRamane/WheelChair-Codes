dae3ccf42066ba957c51cb9a6c3f5f17c794e2f7
notsahilramane <sahil.ramane@somaiya.edu>
2022-11-21 17:32:16 +0530

    Camera-Lidar codes added

--- src/Camera_Lidar_Integration/camera_lidar.py
+++ src/Camera_Lidar_Integration/camera_lidar.py
@@ -4,11 +4,14 @@ import rospy
 
 from ros_numpy import point_cloud2
 from sensor_msgs.msg import PointCloud2
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
-
+import image_geometry
+import ros_numpy
 import copy
 import numpy as np 
+import cv2
+import time
 
 class Integration():
     def __init__(self):
@@ -21,33 +24,42 @@ class Integration():
         self.FOCAL_LENGTH = 0.0028
         self.u0 = 672/2
         self.v0 = 376/2
+        self.CAMERA_MODEL = image_geometry.PinholeCameraModel()
+        self.image_pub = rospy.Publisher("/camera_lidar", Image, queue_size=5)
 
     def subscribeToTopics(self):
         rospy.loginfo("Subscribed to Topics")
+        rospy.Subscriber("/camera/color/camera_info", CameraInfo, self.cameraInfoSave, queue_size=1)
         rospy.Subscriber("/velodyne_points", PointCloud2, self.syncData, queue_size=1, buff_size=2**28)
-        rospy.Subscriber("/left/image_rect_color/compressed", CompressedImage, self.syncImage, queue_size=1, buff_size=2**28)
+        rospy.Subscriber("/camera/color/image_raw/compressed", CompressedImage, self.syncImage, queue_size=1, buff_size=2**28)
 
     def syncData(self, data):
         self.POINTCLOUD_RECEIVED = 1
-        self.pointcloud = point_cloud2.pointcloud2_to_array(data, squeeze=False)
+        self.pointcloud = data
         self.run()
     
     def syncImage(self, data):
         self.IMAGE_RECEIVED = 1
-        self.image = self.bridge.compressed_imgmsg_to_cv2(data)
+        self.image = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
         self.run()
+    
+    def cameraInfoSave(self, msg):
+        self.CAMERA_MODEL.fromCameraInfo(msg)
 
     def run(self):
+        tme = time.time()
         if self.POINTCLOUD_RECEIVED == 1 and self.IMAGE_RECEIVED == 1:
-            lidar_image = copy.deepcopy(self.image)
-            # print(len(lidar_image[0]))
-            for x in self.pointcloud[0]:
-                u = ( self.FOCAL_LENGTH * (x[0] - 1.541 + 0.2) / (x[2] + 0.471) ) + self.u0
-                v = ( self.FOCAL_LENGTH * x[1] / (x[2] + 0.471) ) + self.v0
-                print(u,v)
-                lidar_image[int(v)][int(u)] = x[2] + 0.471
-            self.IMAGE_RECEIVED = 0
-            self.POINTCLOUD_RECEIVED = 0
+            # lidar_image = copy.deepcopy(self.image)
+            print(self.image.shape, "Image shape")
+            points3D = ros_numpy.point_cloud2.pointcloud2_to_array(self.pointcloud)
+            points3D = np.asarray(points3D.tolist())
+
+            points2D = [ self.CAMERA_MODEL.project3dToPixel(point) for point in points3D[:, :3] ]
+            points2D = np.asarray(points2D)
+            
+            print(len(points2D) == len(points3D))
+            print(time.time() - tme)
+            # print(points2D[1200])
 
 def main():
 
